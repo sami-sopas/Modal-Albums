259b0c157e144538bb8f3b22db788f7e548e894d
sami-sopas <ramirezcuenca@outlook.es>
2023-07-13 11:35:38 -0600

    Eliminar imagenes de la carperta y notificaciones implementadas

--- app/music/delete.php
+++ app/music/delete.php
@@ -1,6 +1,7 @@
 <?php
-//Primero llamamos a la conexion
 
+    session_start();
+    //Primero llamamos a la conexion
     require '../config/database.php';
 
     /* Con real_escape_string limpiamos las cadenas que envian los usuarios
@@ -13,7 +14,27 @@
     $sql = "DELETE FROM music WHERE id = $id";
 
      if($conn->query($sql)){
-        //Validar despues
+        
+      //Construccion de la ruta con el nombre
+      $dir = "covers"; //Definimos la carpeta donde se guardaran las imagenes
+
+      $info_img = pathinfo($_FILES['cover']['name']); //Informacion sobre la imagen, regresa un arreglo
+      $info_img['extension']; 
+
+      $cover = $dir . '/'.$id.'.jpg'; //Para construir el nombre y extension de la imagen recibida 
+
+        if(file_exists($cover)){
+          unlink($cover);
+        }
+
+        $_SESSION['color'] = "success";
+        $_SESSION['msg'] = "Registro eliminado";
+     }
+     else {
+
+      $_SESSION['color'] = "danger";
+      $_SESSION['msg'] = "Error al eliminar registro";
+
      }
 
      //Nos volvemos a posicionar en el inicio
